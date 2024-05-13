e1d9164ed5e12257e5354b5c8d7a56ce7f18ebcc
allisonej <eternitymoon@naver.com>
2024-05-13 18:00:32 +0900

    탭 2종 추가
    체중, 키 그래프 탭, BMI그래프 탭

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # -*- coding:utf-8 -*-
 
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 st.title("BMI(Body Mess Index, 체질량지수) 랭크 확인하기")
 st.write("이 페이지에서는 개인 BMI의 위치를 확인합니다.")
@@ -74,7 +78,6 @@ button = st.button('BMI를 체크합니다.')
 
 
 if button:
-    st.caption("처리에 시간이 걸리니 기다려 주세요...")
     target_bmi = input_weight / ( input_height / 100 ) ** 2
     # bmi 구간별로 색 다르게
     if target_bmi<18.5:
@@ -104,15 +107,63 @@ if button:
     # 성별필터링
     # 전체 데이터일 경우에 필터링 안함
     if input_sex == "남성":
-        tmp = tmp[tmp['sex']==1]
+        tmp = tmp[tmp['sex']==1][['height', 'weight']]
     elif input_sex == "여성":
-        tmp = tmp[tmp['sex']==2]
-
+        tmp = tmp[tmp['sex']==2][['height', 'weight']]
+    else:
+        tmp = tmp['height', 'weight']
+    
     # 데이터셋의 BMI 계산
     tmp['BMI'] = tmp['weight'] / ( tmp['height'] / 100 ) ** 2
-    
+
     # 찾고자 하는 값이 몇 퍼센트에 해당하는지 계산
     percentile_value = tmp['BMI'].quantile(q=0.5, interpolation='linear')  # 중간값을 계산합니다.
     percentile = ( tmp['BMI'] < target_bmi ).sum() / len( tmp ) * 100
 
     st.write(f":blue[{target_bmi:.2f}]는 데이터셋에서 약 :blue[{percentile:.2f}] 퍼센트에 해당합니다.")
+
+    # 결과탭; 즉, BUTTON누른 후 유효
+    # 연도, 나이, 성별, 
+    # 탭1 : 키, 체중
+    # 탭3 : bmi
+# 첫 번째 탭: 키와 체중 그래프
+
+    # 탭을 추가
+    tab1, tab2 = st.tabs(['Height vs. Weight', 'BMI Distribution'])
+    
+    with tab1:
+        st.write("## Height vs. Weight")
+        # 플롯 그리기
+        tab1_plot = st.caption("처리에 시간이 걸리니 기다려 주세요...")
+        sns.scatterplot(x='height', y='weight', data=tmp, label='Data Points')
+        plt.scatter(input_height, input_weight, color='red', label='Your Data')
+
+        # 사용자 BMI 표시
+        plt.text(input_height + 1, input_weight - 5, f'BMI: {target_bmi:.2f}', fontsize=10, ha='left')
+
+        plt.xlabel('Height (5cm)')
+        plt.ylabel('Weight (5kg)')
+        plt.title('Height vs. Weight')
+
+        plt.legend()
+        plt.grid(True)
+        tab1_plot = ""
+        st.pyplot()
+
+        tab1_col1, tab1_col2 = st.columns(2)
+        with tab1_col1:
+            st.write(tmp['height'].describe()) 
+        with tab1_col2:
+            st.write(tmp['weight'].describe())
+
+    with tab2:
+        st.write("## BMI Distribution")
+        # 플롯 그리기
+        sns.histplot(tmp['BMI'], kde=True)
+        plt.axvline(target_bmi, color='red', linestyle='--', label='Your BMI')
+        plt.xlabel('BMI')
+        plt.ylabel('Frequency')
+        plt.title('BMI Distribution')
+        plt.legend()
+        plt.text(target_bmi, len(tmp)/(max(tmp['BMI'])-min(tmp['BMI'])), f'Your BMI: {target_bmi:.2f}', fontsize=10, ha='center', va='top')
+        st.pyplot()
